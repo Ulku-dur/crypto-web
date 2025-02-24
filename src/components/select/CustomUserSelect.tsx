1f347d0fc02bcb8a76cab19ad15b61532cf8d136
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2025-02-24 23:42:21 +0300

    Update CustomUserSelect.tsx

--- src/components/select/CustomUserSelect.tsx
+++ src/components/select/CustomUserSelect.tsx
@@ -5,9 +5,7 @@ import Select from "@mui/material/Select";
 const CustomUserSelect = styled((props: any) =>
   <Select {...props} />
 )(({ theme }) => ({
-  "& .MuiOutlinedInput-input::-webkit-input-placeholder": {
-    color: theme.palette.text.secondary,
-    opacity: "0.8"
+ 
   },
   "& .MuiOutlinedInput-input.Mui-disabled::-webkit-input-placeholder": {
     color: theme.palette.text.secondary,
