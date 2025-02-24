28c92f601ab0b8a772aa370a227da496bf8e7b1e
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2025-02-24 23:41:30 +0300

    Update CustomUserSelect.tsx

--- src/components/select/CustomUserSelect.tsx
+++ src/components/select/CustomUserSelect.tsx
@@ -13,9 +13,7 @@ const CustomUserSelect = styled((props: any) =>
     color: theme.palette.text.secondary,
     opacity: "1"
   },
-  "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
-    borderColor: theme.palette.grey[200]
-  }
+ 
 }));
 
 export default CustomUserSelect;
